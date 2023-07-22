8808613f04ca0a7b532d21965ee4f069ee08c5bd
Rickey Weems <weems505@gmail.com>
2023-07-22 00:45:17 -0400

    Added api call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [authors, setAuthors] = useState([])
+    const [poems, setPoem] = useState({
+        author: '',
+        title: '',
+        lines: []
+    })
+
+
+    useEffect(() => {
+        return () => {
+            fetch("https://poetrydb.org/author")
+                .then(r => r.json())
+                .then(author => setAuthors(author.authors))
+
+        };
+    }, [setAuthors]);
+
+    const getRandomInt = () => {
+        return Math.floor(Math.random() * authors.length);
+    }
+
+    const getRandomPoem = () => {
+        const author = authors[getRandomInt()];
+        console.log(author)
+        fetch(`https://poetrydb.org/random/1`)
+            .then(r => r.json())
+            .then(res => {
+                setPoem(poem => ({
+                    ...poem,
+                    author: res[0].author,
+                    title: res[0].title,
+                    lines: res[0].lines
+                }))
+            })
+    }
+
+    return (
+        <>
+            {authors.length === 0 ? <h2>Loading authors</h2>
+                : (poems.author ?
+                    <div className="App">
+                        <h2>{poems.author}</h2>
+                        <h2>{poems.title}</h2>
+                        <br/>
+                        <h4>{poems.lines}</h4>
+                        <button onClick={getRandomPoem}>Get random poem</button>
+                    </div>
+                    : <div className="App">
+                        <button onClick={getRandomPoem}>Get random poem</button>
+                    </div>)
+            }
+        </>
+    );
 }
 
 export default App;
